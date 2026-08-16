73c8c818c93933e26445d17d8083e337684115bb
Daniel Levy <daniel.moreno.levy@gravwell.io>
2026-08-16 14:44:29 -0400

    Push what I have lel

--- exercises/056_unions2.zig
+++ exercises/056_unions2.zig
@@ -35,26 +35,28 @@ const InsectStat = enum { flowers_visited, still_alive };
 const Insect = union(InsectStat) {
     flowers_visited: u16,
     still_alive: bool,
+
+    pub fn printInsect(self: Insect) void {
+        switch (self) {
+            .still_alive => |a| std.debug.print("Ant alive is: {}. ", .{a}),
+            .flowers_visited => |f| std.debug.print("Bee visited {} flowers. ", .{f}),
+        }
+    }
 };
 
 pub fn main() void {
     const ant = Insect{ .still_alive = true };
     const bee = Insect{ .flowers_visited = 16 };
+    const insects = [_]Insect{ant,bee};
 
     std.debug.print("Insect report! ", .{});
 
     // Could it really be as simple as just passing the union?
-    printInsect(???);
-    printInsect(???);
+    for (insects) |i| {
+        i.printInsect();
+    }
 
     std.debug.print("\n", .{});
-}
-
-fn printInsect(insect: Insect) void {
-    switch (???) {
-        .still_alive => |a| std.debug.print("Ant alive is: {}. ", .{a}),
-        .flowers_visited => |f| std.debug.print("Bee visited {} flowers. ", .{f}),
-    }
 }
 
 // By the way, did unions remind you of optional values and errors?
